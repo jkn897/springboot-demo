a661512ceecf96a3f20510716eae625f9432d2a6
jkn897 <jkn897@gmail.com>
2022-11-20 15:09:08 +0530

    packagin and initializer changes added

---
+++ src/main/java/com/mkyong/app/Initializer.java
@@ -0,0 +1,15 @@
+package com.mkyong.app;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import com.mkyong.StartBookApplication;
+
+public class Initializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(StartBookApplication.class);
+	}
+
+}
